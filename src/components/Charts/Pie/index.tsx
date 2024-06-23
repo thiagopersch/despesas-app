633e9e9481a3f0d450c930a49e398643acd88f25
Tiago Persch <thiagopersch@gmail.com>
2024-06-22 20:19:23 -0400

    feat: create components of charts

---
+++ src/components/Charts/Pie/index.tsx
@@ -0,0 +1,22 @@
+import { PieChart } from '@mui/x-charts/PieChart';
+
+const data = [
+  { id: 0, value: 10, label: 'series A' },
+  { id: 1, value: 15, label: 'series B' },
+  { id: 2, value: 20, label: 'series C' },
+];
+
+export default function Pie() {
+  return (
+    <PieChart
+      series={[
+        {
+          data,
+          highlightScope: { faded: 'global', highlighted: 'item' },
+          faded: { innerRadius: 30, additionalRadius: -30, color: 'gray' },
+        },
+      ]}
+      height={200}
+    />
+  );
+}
--- src/templates/Dashboard/index.tsx
+++ src/templates/Dashboard/index.tsx
@@ -1,29 +1,16 @@
-import { Box, List, ListItemText, ListSubheader } from '@mui/material';
+'use client';
+
+import Bars from '@/components/Charts/Bars';
+import Line from '@/components/Charts/Line';
+import Pie from '@/components/Charts/Pie';
+import { Box } from '@mui/material';
 
 export default function Dashboard() {
   return (
-    <Box className="flex flex-col items-center content-center my-10 h-full w-screen">
-      <List
-        subheader={
-          <ListSubheader component="div" id="nested-list-subheader">
-            Lista de coisas para fazer
-          </ListSubheader>
-        }
-      >
-        <del>
-          <ListItemText primary="1. Criar modal de deletar e fazer de forma global" />
-          <ListItemText primary="2. Criar CRUD das categorias" />
-          <ListItemText primary="3. Criar CRUD das prioridades" />
-        </del>
-        <ListItemText primary="4. Criar CRUD dos métodos de pagamentos." />
-        <ListItemText primary="5. Criar CRUD dos status." />
-        <ListItemText primary="6. Criar CRUD dos meses." />
-        <ListItemText primary="7. Criar CRUD dos anos." />
-        <ListItemText primary="8. Criar CRUD das despesas." />
-        <ListItemText primary="8.1. Fazer de alguma forma os seguintes items abaixo:." />
-        <ListItemText primary="8.1.1. Poder parcelar o valor. Ex: 6000 / 12 = 500 por mês" />
-        <ListItemText primary="8.1.2. Poder repetir o valor pela quantidade de parcelas informada. Ex: 1091 * 72 = x valor" />
-      </List>
+    <Box className="flex flex-col items-center content-center h-full w-screen">
+      <Bars />
+      <Pie />
+      <Line />
     </Box>
   );
 }
